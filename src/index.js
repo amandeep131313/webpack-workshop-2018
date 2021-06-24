863404723d9985efbb447114ab251a9b0d5e5e89
Aman Deep <47072498+amandeep131313@users.noreply.github.com>
2021-06-24 23:25:55 +0530

    add changes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import nav from './nav'
+import makeButton from './button'
+import {add} from './button-styles'
+console.log("hello"+nav);
+console.log('makeButton',makeButton("my first button!"));
+console.log('add',add);
